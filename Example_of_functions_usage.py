658cf0c43288da54c459b944dbc5646f0b166cf0
Greta Grassmann <gretagrassmann0@gmail.com>
2021-05-05 16:50:11 +0200

    Cleaned up version.

--- Example_of_functions_usage.py
+++ Example_of_functions_usage.py
@@ -19,17 +19,6 @@ Dpp = 1       # the distance between points of the same patch (needed to remove 
 ZOrder = 20   # the Zernike expansion order..
 
 
-############## Processing protein A surface.. ################################
-#print("Di che parte di RRM2 si tratta: W per intera, 208 per taglio a 208, 219 per taglio a 219")
-#proteina=input()
-#if proteina == "W":
-#    loc = "../RRM2/"
-#elif proteina == "208":
-#    loc = "../208/"
-#elif proteina == "219":
-#    loc = "../219/"
-
-#surf_name_a = "%scluster1.dms" % loc
 surf_name_a = "../RRM2/cluster1.dms"
 
 surf_a_ = pd.read_csv(surf_name_a)
@@ -47,18 +36,11 @@ surf_a_obj = SF.Surface(surf_a[:,:], patch_num = 0, r0 = Rs, theta_max = 45)
 res1, c = SF.ConcatenateFigPlots([surf_a_obj.surface[:,:3]])
 SF.Plot3DPoints(res1[:,0], res1[:,1], res1[:,2], c, 0.3)
         
-#SF.ConcatenateFigPlots?
-#SF.Plot3DPoints?
-#SF.Surface.PatchReorientNew?
-#SF.Surface?
-
-
 ## To isolate one patch of the surface:
 ## Example, the patch around point 5000..
 patch, mask = surf_a_obj.BuildPatch(point_pos=5000, Dmin=.5)
-        
 
-## To rotate a patch:    
+## To rotate a patch:
 rot_patch, rot_patch_nv = surf_a_obj.PatchReorientNew(patch, 1)
 
 
@@ -81,9 +63,6 @@ SF.Plot3DPointsAndVectors(rot_patch[:,0], rot_patch[:,1], rot_patch[:,2],rot_pat
 
 #############################################
     
-
-#SF.Plot3DPointsAndVectors?
-
 
 ## To project the patch on the xy plane...
 
@@ -113,29 +92,6 @@ mpl.show()
 zernike_env = ZF.Zernike2d(new_plane_)
 
 
-# zernike_env.img = new_plane_
-
-#fig, ax = mpl.subplots(1,2, dpi = 150)
-#ax[0].imshow(zernike_env.r)
-#ax[1].imshow(zernike_env.t)
-#ax[0].set_yticklabels([])
-#ax[0].set_xticklabels([])
-#ax[1].set_yticklabels([])
-#ax[1].set_xticklabels([])
-
-#ax[0].set_xlabel("x")
-#ax[0].set_ylabel("y")
-#ax[1].set_xlabel("x")
-#ax[1].set_ylabel("y")
-
-#ax[0].set_title("$\\rho$")
-#ax[1].set_title("$\\theta$")
-#mpl.tight_layout()
-#mpl.show()
-
-
-#zernike_env.ComputeMoment?
-
 
 ### To compute and visualize a complex Zernike polynom...
 
@@ -164,7 +120,6 @@ recon, coeff = zernike_env.ZernikeRecostruction(order=ZOrder, PLOT=0)
 ### ZernikeRecostruction returns also the reconstructed plane, so it is slow. Use ZernikeDecomposition, which returns only coeff but it is much faster.
 
 
-#coeff = zernike_env.ZernikeDecomposition(order=ZOrder)
 
         
 
@@ -196,4 +151,3 @@ mpl.plot(coeff_inv)
 mpl.xlabel("index")
 mpl.ylabel("Invariant")
 mpl.show()
-
