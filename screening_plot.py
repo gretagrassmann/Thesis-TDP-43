8a949594ae4709419a43eae54a1a6a44382b5cf0
Greta Grassmann <gretagrassmann0@gmail.com>
2021-05-07 18:12:50 +0200

    Final version.

--- screening_plot.py
+++ screening_plot.py
@@ -9,35 +9,42 @@ from scipy.spatial import distance_matrix
 import ZernikeFunc as ZF
 import SurfaceFunc as SF
 
-Rs = 2.  # the radius of the sphere that includes the patch..
+Rs_select = 1.  # the radius of the sphere that includes the patch..
 
 
 pdb_file = "..\RRM2\cluster2.dms"
 surf_name = pdb_file
 
-#TO VISUALIZE THE WHOLE MOLECULE
-surf_total = pd.read_csv(surf_name)
-tot_len = len(surf_total["x"])
-surf_tot = np.zeros((tot_len, 6))
-surf_tot[:, :] = surf_total[["x", "y", "z", "Nx", "Ny", "Nz"]]
-
 with open("..\RRM2\cluster2\positive_new_sampling\index_possible_area.txt") as f:
     index_possible_area = [int(float(x)) for x in f.read().split()]
 
+#TO VISUALIZE THE WHOLE MOLECULE
+surf_total1 = pd.read_csv(surf_name)
+tot_len = len(surf_total1["x"])
+r_t = [i for i in range(tot_len) if i in index_possible_area]
+surf_total = pd.read_csv(surf_name, skiprows=r_t)
+len_tot_partial = len(surf_total["x"])
+
 ###PLOT TO VISUALIZE WHICH POINT WHERE SELECTED AS CENTER OF THE PATCHES
-l_a = len(index_possible_area)-1
-print("l_a=", l_a)
-r = [i for i in range(total_len) if i not in index_possible_area]
-surf_select = pd.read_csv(surf_name, skiprows=r)
-surf_a = np.zeros((l_a, 6))
+l_a = len(index_possible_area)
+print("number of considered points=", l_a)
+print("number of total points in the surface=", tot_len)
+r = [i for i in range(tot_len) if i not in index_possible_area]
+
+colnames=["x", "y", "z", "Nx", "Ny", "Nz"]
+surf_select = pd.read_csv(surf_name, names=colnames, header=None, skiprows=r)
+l = len(surf_select["x"])
+surf_a = np.zeros((l, 6))
 surf_a[:, :] = surf_select[["x", "y", "z", "Nx", "Ny", "Nz"]]
 
+surf_tot = np.zeros((len_tot_partial, 6))
+surf_tot[:, :] = surf_total[["x", "y", "z", "Nx", "Ny", "Nz"]]
 
 
 
 ### To inizialize the Surface class:
-surf_a_obj = SF.Surface(surf_a[:, :], patch_num=0, r0=Rs, theta_max=45)
-surf_tot_obj = SF.Surface(surf_tot[:, :], patch_num=0, r0=Rs, theta_max=45)
+surf_a_obj = SF.Surface(surf_a[:, :], patch_num=0, r0=Rs_select, theta_max=45)
+surf_tot_obj = SF.Surface(surf_tot[:, :], patch_num=0, r0=Rs_select, theta_max=45)
 ### To plot surface in 3D:
 res1, c = SF.ConcatenateFigPlots([surf_tot_obj.surface[:, :3], surf_a_obj.surface[:, :3]])
 
