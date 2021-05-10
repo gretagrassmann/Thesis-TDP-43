6a5fdf7c0fdfc03ad8248e07d5b3c5155c4af55c
Greta Grassmann <gretagrassmann0@gmail.com>
2021-05-10 16:16:51 +0200

    Crown used for any patch, except when scal prod = 0.

--- screening_crown.py
+++ screening_crown.py
@@ -6,7 +6,7 @@ import os, sys
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as mpl
-
+import math
 import pandas as pd
 from scipy.spatial import distance_matrix
 
@@ -28,9 +28,9 @@ name_ = "Cluster 2 of RRM2 EXPERIMENT"
 Npoint = int(1)
 
 Rs_select = 4.  # the radius of the sphere that includes the patch..
-Max_scalar  = 1.e-60
+#Max_scalar  = 1.e-60
 R_crown = 1.e60
-
+divided = 2000
 # loading surface file..
 
 surf_name = pdb_file
@@ -57,23 +57,30 @@ for i in index_possible_area:
     prod = patch[0,3:6]
     for j in range(1,len(patch)):
         prod = prod*patch[j,3:6]
-    prod_scal_patch = sum(prod) #QUESTO MI DICE QUANTO LA PATCH E' PIATTA
-    if abs(prod_scal_patch) > Max_scalar: # SALVO GLI INDICI DEI PUNTI DOVE SERVE CAMPIONAMENTO PIU' FITTO
-       # A SECONDA DI QUANTO LA PATCH E' PIATTA, SALVO GLI INDICI SOLO DI UNA CORONA STRETTA SUL BORDO DELLA PATCH
+    prod_scal_patch = abs(sum(prod)) #QUESTO MI DICE QUANTO LA PATCH E' PIATTA
+    #if prod_scal_patch != 0.0:
+    #    print(abs(math.log10(prod_scal_patch)))
+    #if prod_scal_patch > Max_scalar: # SALVO GLI INDICI DEI PUNTI DOVE SERVE CAMPIONAMENTO PIU' FITTO
+    if prod_scal_patch != 0.0:
+           # A SECONDA DI QUANTO LA PATCH E' PIATTA, SALVO GLI INDICI SOLO DI UNA CORONA STRETTA SUL BORDO DELLA PATCH
         point_index = np.zeros((len(patch)))
         d2 = np.zeros(len(patch))
         center_index = []
         for k in range(len(patch)):
             point_index[k] = np.where((surf[:, 0] == patch[k, 0]) & (surf[:, 1] == patch[k, 1]) & (surf[:, 2] == patch[k, 2]))[0]
             d2[k] = (surf[int(point_index[k]),0] - surf[i,0])**2 + (surf[int(point_index[k]),1] - surf[i,1])**2 + (surf[int(point_index[k]),2] - surf[i,2])**2
-            #print("d2[{}]=".format(k), d2[k])
-###CAMBIA DA > A <
-            if d2[k] < abs(prod_scal_patch)*R_crown: #SALVO I PUNTI NEL CENTRO
+            #print("d[{}]=".format(k), d2[k])
+            #print("value", abs((Rs_select / divided) * math.log10(prod_scal_patch)))
+            if d2[k] < abs((Rs_select/divided)*math.log10(prod_scal_patch)): #SALVO I PUNTI NEL CENTRO
                 center_index.append(point_index[k])
+
 
 
         # STO TENENDO COME PUNTI DI ZERNIKE ANCHE QUELLI NELLA CORONA DELLE PARTI PIATTE
         index_possible_area = [int(i) for i in index_possible_area if i not in center_index]
 
-
+        #if len(patch) != len(center_index) & len(center_index) != 0:
+        #    print("points in the patch", len(patch))
+        #    print("points to be discarded", len(center_index))
+print("tot=", len(index_possible_area))
 np.savetxt("{}\index_possible_area.txt".format(respath), index_possible_area)
