bbdc38a477ffe4960ddf90b38cc5aacadcb467b5
Greta Grassmann <gretagrassmann0@gmail.com>
2021-05-20 09:26:30 +0200

    Latest version.

--- screening_plot.py
+++ screening_plot.py
@@ -1,34 +1,38 @@
-import os, sys
 import numpy as np
-import scipy as sp
-import matplotlib.pyplot as mpl
-import matplotlib.pyplot as plt
-
 import pandas as pd
-from scipy.spatial import distance_matrix
-
-import ZernikeFunc as ZF
 import SurfaceFunc as SF
-import my_functions
 
-Rs_select = 1.  # the radius of the sphere that includes the patch..
+    ##########  PARAMETERS  ##########
+
+with open('configuration.txt') as f:
+          for line in f:
+              exec(line)
+alpha = 0.075
+
+#Rs_select = 6  # the radius of the sphere that includes the patch..
+#R_zernike = 6
+#alpha = 0.075
+#fragment = 208
+#luster = 1
+#tep = 1
 
 
-pdb_file = "..\RRM2\cluster2.dms"
-surf_name = pdb_file
 
-with open("..\RRM2\cluster2\R_s_8\index\index_possible_area_R_s_8.0_alpha_0.078_step_1_points_2161.txt") as f:
+    ############### END PARAMETERS  ##############
+pdb_file = "..\\{}\cluster{}.dms".format(fragment, cluster)
+respath = "..\\{}\cluster{}\R_zernike_{}\R_s_{}".format(fragment, cluster, R_zernike, Rs_select)
+
+with open("{}\index\index_possible_area_R_s_{}_alpha_{}_step_{}.txt".format(respath,Rs_select, alpha, step)) as f:
     index_possible_area = [int(float(x)) for x in f.read().split()]
 
 #TO VISUALIZE THE WHOLE MOLECULE
-surf_total1 = pd.read_csv(surf_name)
+surf_total1 = pd.read_csv(pdb_file)
 tot_len = len(surf_total1["x"])
 #r_t = [i for i in range(tot_len) if (i in index_possible_area) & (i != 0)]
 r_t = [i+1 for i in range(tot_len) if (i in index_possible_area)] #la prima riga me la vede vuota
 
 
-surf_total = pd.read_csv(surf_name, skiprows=r_t)
-print(surf_total)
+surf_total = pd.read_csv(pdb_file, skiprows=r_t)
 len_tot_partial = len(surf_total["x"])
 
 ###PLOT TO VISUALIZE WHICH POINT WHERE SELECTED AS CENTER OF THE PATCHES
@@ -39,7 +43,7 @@ r = [i for i in range(tot_len) if (i not in index_possible_area) & (i!=0)]
 
 #colnames=["x", "y", "z", "Nx", "Ny", "Nz"]
 #surf_select = pd.read_csv(surf_name, names=colnames, header=None, skiprows=r)
-surf_select = pd.read_csv(surf_name, skiprows=r)
+surf_select = pd.read_csv(pdb_file, skiprows=r)
 l = len(surf_select["x"])
 surf_a = np.zeros((l, 6))
 surf_a[:, :] = surf_select[["x", "y", "z", "Nx", "Ny", "Nz"]]
