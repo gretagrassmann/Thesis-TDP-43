72233c17cef5fc03ba601fba3cc4463d0db3b820
gretagrassmann <61978037+gretagrassmann@users.noreply.github.com>
2021-06-04 09:11:27 +0200

    See report 03/06

--- comparison.py
+++ comparison.py
@@ -14,10 +14,16 @@ screening = "index_continuous_distribution_exp"
 
 #number_crowns = 5
 
-number_division = 60
+number_division = 20
 
 #alpha = [.05, .1, .15, .2, .25, .3, .35, .4, .45, .5, .55, .6, .65, .7, .75, .8, .85, .9, .95, 1.]
-alpha = [1., 2., 3., 4., 5., 6., 7., 8., 9., 10., 11., 12., 13., 14., 15., 16., 17., 18., 19., 20.]
+alpha = [30., 60., 90., 120., 150., 180., 210., 240., 270., 300., 330., 360., 390.,
+         420., 450., 480., 510., 540., 570., 600., 630., 660.,
+         690., 720., 750., 780., 810., 840., 870., 900.,
+         930., 960., 990.]
+
+
+
 
 alpha_plot = []
 total_plot = 0 #1 se vuoi vederlo
@@ -60,6 +66,11 @@ if total_plot == 1:
     sc = plt.scatter(tot_x_ax, tot_y_ax, c=z_total, cmap=cm)
     plt.colorbar(sc, format='%.1f', label= "Mean cosine value of the patch")
     #plt.show()
+
+
+elipse_tot = []
+variance_tot = []
+free_cells_tot = []
 
 reward = []
 alpha_points = []
@@ -114,6 +125,8 @@ for a in alpha:
     common_points = []
     alpha_points_for_variance = []
 
+
+
     k = 0
     occupied_cells = 0
     for i in range(len(x_grid)):
@@ -134,27 +147,58 @@ for a in alpha:
             if count_cell_alpha[k] != 0:
                 occupied_cells +=1
                 common_points.append(abs(count_cell_total[k]-count_cell_alpha[k]))
-                alpha_points_for_variance.append(count_cell_alpha[k]*number_points)
+                alpha_points_for_variance.append(len(index_cell_alpha[0]))
             k += 1
 
 
     #reward.append(abs(tot_elips-alpha_elips)*(sum(z)/number_points)*sum(common_points)*((len(x_grid)*(len(y_grid)))-occupied_cells)/number_points)
-    reward.append(abs(tot_elips-alpha_elips)*(sum(z)/number_points)*statistics.variance(alpha_points_for_variance)*((len(x_grid)*len(y_grid))-occupied_cells)/(len(x_grid)*(len(y_grid))))
+
+#    reward.append(abs(tot_elips-alpha_elips)*(sum(z)/number_points)*(statistics.variance(alpha_points_for_variance)/number_points)*((len(x_grid)*len(y_grid))-occupied_cells)/(len(x_grid)*(len(y_grid))))
+    reward.append((sum(z)/number_points)*(statistics.variance(alpha_points_for_variance)/number_points)*((len(x_grid)*len(y_grid))-occupied_cells)/(len(x_grid)*(len(y_grid))))
     points_cosine.append(sum(z)/number_points)
+
+    elipse_tot.append(abs(tot_elips-alpha_elips))
+    variance_tot.append(statistics.variance(alpha_points_for_variance)/number_points)
+    free_cells_tot.append(((len(x_grid)*len(y_grid))-occupied_cells)/(len(x_grid)*(len(y_grid))))
 
 #np.savetxt("{}\\reward_function_continuous_distribution_exp.txt".format(respath),reward)
 
 
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
+ax1.set_xlabel('$\\alpha$ value')
+ax1.set_ylabel("components")
+plt.plot(alpha, variance_tot, label = 'variance')
+plt.plot(alpha, elipse_tot, label = 'elipse')
+plt.plot(alpha, free_cells_tot, label = 'not occupied')
+plt.plot(alpha, points_cosine, label = 'mean cosine')
+leg = ax1.legend()
+
+
+
+######### Qua trovo i minimi locali ##############
+#alpha_min_loc = []
+#loss_min_loc = []
+#for i in range(1,len(alpha)-1):
+#    if (reward[i-1] > reward[i]) & (reward[i]<reward[i+1]):
+#        alpha_min_loc.append(alpha[i])
+#        loss_min_loc.append(reward[i])
+
+loss_min = np.min(reward)
+alpha_min = np.where(loss_min == reward)[0][0]
+mask = np.array(reward) == loss_min
+color = np.where(mask, 'red', 'blue')
+
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
 #ax1.set_title("Cost function")
 ax1.set_xlabel('$\\alpha$ value')
 ax1.set_ylabel("Loss function ")
-#ax1.set_xticks(np.arange(0, 1.05, .05))
-for i in range(len(alpha)):
-    plt.plot(alpha[i], reward[i], 'bo')
-   # plt.text(alpha[i] * (1 + 0.01), reward[i] * (1 + 0.01), "{} points".format(alpha_points[i]))
-leg = ax1.legend()
+#ax1.scatter(alpha, reward, color=color)
+ax1.plot(alpha, reward)
+ax1.plot(alpha[alpha_min], reward[alpha_min], '*')
+ax1.legend(["The minimum of the Loss function is at $\\alpha$={}".format(alpha[alpha_min])])
+
 
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
@@ -168,6 +212,8 @@ leg = ax1.legend()
 #plt.show()
 
 plt.show()
+
+
 
 
 print("Qual'e' il miglior valore per alpha?")
